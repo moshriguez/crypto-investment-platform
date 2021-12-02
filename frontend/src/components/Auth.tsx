7a26c4719d731759676a5989ea0f74613b58f53c
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-12-01 16:42:41 -0800

    create signup/signin form

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Avatar, Button, Container, Grid, Paper, Typography } from '@mui/material'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+import Input from './Input'
+// Types
+import { User } from '../types'
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirm: '' }
+
+interface AuthProps {
+    setUser: (arg: User | null) => void
+}
+
+const Auth: React.FC<AuthProps> = ({ setUser }) => {
+    const navigate = useNavigate()
+    const [formData, setFormData] = useState(initialState);
+    const [isSignUp, setIsSignUp] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const handleShowPassword = () => setShowPassword(!showPassword);
+
+    const switchForm = () => {
+        setFormData(initialState)
+        setIsSignUp(!isSignUp)
+        setShowPassword(false)
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const url = 'http://localhost:5000/users/'
+        const configObj = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        }
+        try {
+            let res
+            if(isSignUp) {
+                res = await fetch(url + 'signup', configObj)
+            } else {
+                res = await fetch(url + 'signin', configObj)
+            }
+            const data = await res.json()
+            localStorage.setItem("jwt", data.token)
+            setUser(data.result)
+            //TODO change to watch list once built
+            navigate('/')          
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+  
+    return (
+        <Container component='main' maxWidth='xs'>
+            <Paper elevation={3}>
+                <Avatar>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component='h5' variant='h5'>{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
+                <form onSubmit={handleSubmit}>
+                    <Grid container spacing={2}>
+                        {isSignUp && (
+                            <>
+                                <Input 
+                                    name='firstName'
+                                    label='First Name'
+                                    value={formData.firstName}
+                                    handleChange={handleChange}
+                                    half
+                                />
+                                <Input 
+                                    name='lastName'
+                                    label='Last Name'
+                                    value={formData.lastName}
+                                    handleChange={handleChange}
+                                    half
+                                />
+                            </>
+                        )}
+                        <Input 
+                            name='email'
+                            label='Email'
+                            value={formData.email}
+                            handleChange={handleChange}
+                            type='email'
+                        />
+                        <Input 
+                            name='password'
+                            label='Password'
+                            value={formData.password}
+                            handleChange={handleChange}
+                            type={showPassword ? 'text' : 'password'}
+                            handleShowPassword={handleShowPassword}
+                        />
+                        {isSignUp && (
+                            <Input 
+                                name='confirm'
+                                label='Confirm Password'
+                                value={formData.confirm}
+                                handleChange={handleChange}
+                                type='password'
+
+                            />
+                        )}
+                    </Grid>
+                    <Button 
+                        type='submit'
+                        fullWidth
+                        variant='contained'
+                        color='primary'
+                    >
+                        {isSignUp ? 'Sign Up' : 'Sign In'}
+                    </Button>
+                    <Grid container justifyContent='flex-end'>
+                        <Grid item>
+                            <Button onClick={switchForm}>
+                                {isSignUp ? 'Already have an account? Sign in' : "Don't have an account? Sign up"}
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
+    )
+}
+
+export default Auth
